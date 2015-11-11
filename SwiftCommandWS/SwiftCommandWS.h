c084213a2dc559c7b2dcc647a4cf3ae39db2af17
FCO <fernandocorrea@gmail.com>
2015-11-10 23:32:14 -0200

    Initial Commit

---
+++ SwiftCommandWS/SwiftCommandWS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCommandWS.h
+//  SwiftCommandWS
+//
+//  Created by Fernando Oliveira on 10/11/15.
+//  Copyright © 2015 FCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCommandWS.
+FOUNDATION_EXPORT double SwiftCommandWSVersionNumber;
+
+//! Project version string for SwiftCommandWS.
+FOUNDATION_EXPORT const unsigned char SwiftCommandWSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCommandWS/PublicHeader.h>
+
+
